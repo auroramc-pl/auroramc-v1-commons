4e803a388402ab9fde9f40b1ad5aaf8e634eecf5
Rafał Chomczyk <hello@rafal.moe>
2024-04-02 11:28:19 +0200

    Add caught exception delegation in scheduler

--- auroramc-commons-core/src/main/java/pl/auroramc/commons/scheduler/Scheduler.java
+++ auroramc-commons-core/src/main/java/pl/auroramc/commons/scheduler/Scheduler.java
@@ -1,36 +1,41 @@
 package pl.auroramc.commons.scheduler;
 
 import static java.time.Duration.ZERO;
+import static pl.auroramc.commons.scheduler.SchedulerUtils.NOTHING;
 
 import com.pivovarit.function.ThrowingSupplier;
 import java.time.Duration;
 import java.util.concurrent.CompletableFuture;
+import pl.auroramc.commons.CompletableFutureUtils;
 
 public interface Scheduler {
 
   default CompletableFuture<Void> run(final SchedulerPoll pool, final Runnable task) {
     return supply(
-        pool,
-        () -> {
-          task.run();
-          return null;
-        });
+            pool,
+            () -> {
+              task.run();
+              return NOTHING;
+            })
+        .exceptionally(CompletableFutureUtils::delegateCaughtException);
   }
 
   default CompletableFuture<Void> runLater(
       final SchedulerPoll pool, final Duration delay, final Runnable task) {
     return supplyLater(
-        pool,
-        delay,
-        () -> {
-          task.run();
-          return null;
-        });
+            pool,
+            delay,
+            () -> {
+              task.run();
+              return NOTHING;
+            })
+        .exceptionally(CompletableFutureUtils::delegateCaughtException);
   }
 
   default <T> CompletableFuture<T> supply(
       final SchedulerPoll pool, final ThrowingSupplier<T, Exception> supplier) {
-    return supplyLater(pool, ZERO, supplier);
+    return supplyLater(pool, ZERO, supplier)
+        .exceptionally(CompletableFutureUtils::delegateCaughtException);
   }
 
   <T> CompletableFuture<T> supplyLater(
@@ -38,6 +43,5 @@ public interface Scheduler {
       final Duration delay,
       final ThrowingSupplier<T, Exception> supplier);
 
-  void schedule(
-      final SchedulerPoll poll, final Runnable task, final Duration period);
+  void schedule(final SchedulerPoll poll, final Runnable task, final Duration period);
 }
---
+++ auroramc-commons-core/src/main/java/pl/auroramc/commons/scheduler/SchedulerUtils.java
@@ -0,0 +1,9 @@
+package pl.auroramc.commons.scheduler;
+
+final class SchedulerUtils {
+
+  // That is a neat trick to help compiler infer the type of the lambda expression
+  static final Void NOTHING = null;
+
+  private SchedulerUtils() {}
+}
