fff386f6caef903119dce1b17b12c01470559b3f
Rafał Chomczyk <hello@rafal.moe>
2024-04-02 20:48:28 +0200

    Add utility methods for between range and fix accessibility to matches method

--- auroramc-commons-core/src/main/java/pl/auroramc/commons/range/Between.java
+++ auroramc-commons-core/src/main/java/pl/auroramc/commons/range/Between.java
@@ -2,7 +2,15 @@ package pl.auroramc.commons.range;
 
 public record Between<T extends Comparable<T>>(T maximum, T minimum) {
 
-  boolean matches(final T query) {
+  public static <T extends Comparable<T>> Between<T> single(final T value) {
+    return new Between<>(value, value);
+  }
+
+  public static <T extends Comparable<T>> Between<T> ranged(final T minimum, final T maximum) {
+    return new Between<>(maximum, minimum);
+  }
+
+  public boolean matches(final T query) {
     return query.compareTo(maximum) <= 0 && query.compareTo(minimum) >= 0;
   }
 }
