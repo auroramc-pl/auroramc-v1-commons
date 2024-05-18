567ca8b5c5ad6634f3cba8a784d53549e7766b28
Rafał Chomczyk <hello@rafal.moe>
2024-05-18 18:39:40 +0200

    Add command builder processor for a velocity platform

---
+++ auroramc-commons-platforms/auroramc-commons-platform-velocity/src/main/java/pl/auroramc/commons/velocity/integration/commands/VelocityCommandsBuilderProcessor.java
@@ -0,0 +1,48 @@
+package pl.auroramc.commons.velocity.integration.commands;
+
+import com.velocitypowered.api.command.CommandSource;
+import com.velocitypowered.api.proxy.Player;
+import com.velocitypowered.api.proxy.ProxyServer;
+import dev.rollczi.litecommands.LiteCommandsBuilder;
+import dev.rollczi.litecommands.LiteCommandsInternal;
+import dev.rollczi.litecommands.permission.MissingPermissions;
+import dev.rollczi.litecommands.velocity.LiteVelocitySettings;
+import dev.rollczi.litecommands.velocity.tools.VelocityOnlyPlayerContextual;
+import pl.auroramc.commons.integration.commands.CommandsBuilderProcessor;
+import pl.auroramc.commons.integration.configs.command.CommandMessageSource;
+import pl.auroramc.commons.velocity.integration.commands.argument.PlayerArgumentResolver;
+import pl.auroramc.commons.velocity.integration.commands.permission.DefaultMissingPermissionsHandler;
+import pl.auroramc.messages.message.compiler.MessageCompiler;
+
+public class VelocityCommandsBuilderProcessor
+    extends CommandsBuilderProcessor<CommandSource, LiteVelocitySettings> {
+
+  private final ProxyServer server;
+  private final CommandMessageSource messageSource;
+  private final MessageCompiler<CommandSource> messageCompiler;
+
+  public VelocityCommandsBuilderProcessor(
+      final ProxyServer server,
+      final CommandMessageSource messageSource,
+      final MessageCompiler<CommandSource> messageCompiler) {
+    super(messageSource, messageCompiler);
+    this.server = server;
+    this.messageSource = messageSource;
+    this.messageCompiler = messageCompiler;
+  }
+
+  @Override
+  public void process(
+      final LiteCommandsBuilder<CommandSource, LiteVelocitySettings, ?> builder,
+      final LiteCommandsInternal<CommandSource, LiteVelocitySettings> internal) {
+    super.process(builder, internal);
+    builder
+        .context(
+            Player.class,
+            new VelocityOnlyPlayerContextual<>(messageSource.executionFromConsoleIsUnsupported))
+        .result(
+            MissingPermissions.class,
+            new DefaultMissingPermissionsHandler<>(messageSource, messageCompiler))
+        .argument(Player.class, new PlayerArgumentResolver<>(server, messageSource));
+  }
+}
