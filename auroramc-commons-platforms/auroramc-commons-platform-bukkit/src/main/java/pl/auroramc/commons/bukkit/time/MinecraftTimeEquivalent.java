0ac015167ddb4e38588c15378bc0a490e8dac757
Rafał Chomczyk <hello@rafal.moe>
2024-05-18 18:42:47 +0200

    Fix code style inconsistencies

--- auroramc-commons-platforms/auroramc-commons-platform-bukkit/src/main/java/pl/auroramc/commons/bukkit/time/MinecraftTimeEquivalent.java
+++ auroramc-commons-platforms/auroramc-commons-platform-bukkit/src/main/java/pl/auroramc/commons/bukkit/time/MinecraftTimeEquivalent.java
@@ -9,7 +9,7 @@ public class MinecraftTimeEquivalent {
   private static final long MILLISECONDS_PER_TICK = MILLISECONDS_PER_SECOND / TICKS_PER_SECOND;
 
   public static final long TICK = 1;
-  public static final long SECOND = 20;
+  public static final long SECOND = TICK * 20;
   public static final long MINUTE = SECOND * 60;
   public static final long HOUR = MINUTE * 60;
 
