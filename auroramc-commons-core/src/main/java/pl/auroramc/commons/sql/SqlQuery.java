119ef6e3ea6b4caf7d44cf62361fc270a2f15cb2
shitzuu <hello@rafal.moe>
2024-08-12 10:54:43 +0200

    Use proper retention for SqlQuery annotation

--- auroramc-commons-core/src/main/java/pl/auroramc/commons/sql/SqlQuery.java
+++ auroramc-commons-core/src/main/java/pl/auroramc/commons/sql/SqlQuery.java
@@ -1,13 +1,13 @@
 package pl.auroramc.commons.sql;
 
 import static java.lang.annotation.ElementType.FIELD;
-import static java.lang.annotation.RetentionPolicy.RUNTIME;
+import static java.lang.annotation.RetentionPolicy.CLASS;
 
 import java.lang.annotation.Retention;
 import java.lang.annotation.Target;
 import org.intellij.lang.annotations.Language;
 
-@Retention(RUNTIME)
 @Target(FIELD)
+@Retention(CLASS)
 @Language("MariaDB")
 public @interface SqlQuery {}
